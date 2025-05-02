1f80010e665f8837b962e58a76686b91d4f06c10
Dwight Ledet <dwightledet@protonmail.com>
2025-05-02 10:13:05 +0200

    frontend

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Form from '../components/form'
+
+const Login = () => {
+  return (
+    <Form route="api/token/" method="login" />
+  )
+}
+
+export default Login
